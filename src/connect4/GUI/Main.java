b5b381f18f3bdfaa2d4912dd8cf5046f29021b31
Hanpei Fang <hanpeifang@ruri.waseda.jp>
2020-12-01 04:38:09 +0800

    Update Main.java

--- src/connect4/GUI/Main.java
+++ src/connect4/GUI/Main.java
@@ -5,14 +5,13 @@ import connect4.Core.GameState;
 import connect4.Core.PlayerFactory;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.Label;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +23,9 @@ public class Main extends Application {
         launch(args);
     }
 
+    /**
+     * @source  https://stackoverflow.com/questions/12153622/how-to-close-a-javafx-application-on-window-close
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         List<String> playerTypes = new ArrayList<>();
@@ -92,7 +94,7 @@ public class Main extends Application {
 
         Scene scene = new Scene(root, 1030, 740);
         primaryStage.setScene(scene);
-        
+
         primaryStage.setOnCloseRequest(windowEvent -> {
             Platform.exit();
             System.exit(0);
