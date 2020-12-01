8ea03dd46f9752bc6745aa3d9e0e02e2dac7d7cc
Hanpei Fang <hanpeifang@ruri.waseda.jp>
2020-12-02 04:29:29 +0800

    Adding Launcher

---
+++ src/connect4/GUI/Launcher.java
@@ -0,0 +1,10 @@
+package connect4.GUI;
+
+/**
+ * @source https://taylorial.com/cs1021/Jar.htm#jarwithjavafxpackagesincluded
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
