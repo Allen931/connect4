afd323b74be77a8430ad0f34ac8665ea1ec6415d
Hanpei Fang <hanpeifang@ruri.waseda.jp>
2020-12-02 14:56:47 +0800

    Add support to Java 11

--- src/connect4/Core/PlayerFactory.java
+++ src/connect4/Core/PlayerFactory.java
@@ -2,10 +2,20 @@ package connect4.Core;
 
 public class PlayerFactory {
     public static GamePlayer create(int type, String name, int piece) throws Exception {
-        return switch (type) {
-            case 1 -> new HumanPlayer(name, piece);
-            case 2 -> new AIPlayer(name, piece);
-            default -> throw new Exception("Invalid Player Type");
-        };
+        // support Java 11
+        switch (type) {
+            case 1:
+                return new HumanPlayer(name, piece);
+            case 2:
+                return new AIPlayer(name, piece);
+            default:
+                throw new Exception("Invalid Player Type");
+        }
+        // Java 14
+//        return switch (type) {
+//            case 1 -> new HumanPlayer(name, piece);
+//            case 2 -> new AIPlayer(name, piece);
+//            default -> throw new Exception("Invalid Player Type");
+//        };
     }
 }
